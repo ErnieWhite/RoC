9fed4dcfadd99cc3144c2b16f9b29a81764063d3
Ernie White <ernie.white.68@gmail.com>
2023-07-02 10:02:08 -0500

    day 5 part 1 complete

--- Day05/day05.py
+++ Day05/day05.py
@@ -1,5 +1,21 @@
+from hashlib import md5
+from binascii import hexlify
+
+
 def main():
-    pass
+    message = 'ffykfhsq' 
+    
+    password = [] 
+
+    i = 0 
+    while len(password) < 8:
+        test_message = message + str(i)
+        result = md5(test_message.encode('utf-8'))
+        hexed = hexlify(result.digest()).decode()
+        if hexed.startswith('00000'):
+            password.append(hexed[5])
+        i += 1
+    print(i, test_message, ''.join(password), hexed)
 
 
 if __name__ == '__main__':
