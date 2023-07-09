da4a5a813659570f6d5aa47418b2ac914109ad89
Ernie White <ernie.white.68@gmail.com>
2023-07-08 22:15:09 -0500

    initial commit for day 8

---
+++ Day08/main.py
@@ -0,0 +1,22 @@
+# Advent of Code - 2016
+# --- Day 8: Two-Factor Authentication ---
+
+def get_input(f):
+    with open(f, 'r', encoding='utf-8') as file:
+        data = file.read().splitlines()
+    return data
+
+def solver1(data):
+    pass
+
+def solver2(data):
+    pass
+
+def main():
+    data = get_input('input.txt')
+    solver1(data)
+    solver2(data)
+
+
+if __name__ == '__main__':
+    main()
